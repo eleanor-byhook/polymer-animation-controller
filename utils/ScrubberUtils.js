7da9eeb2715200394b4fe63bda0799f6588b3e02
Eleanor <eleanor.weigert@byhook.com>
2016-05-05 11:28:21 -0700

    Functional data passing between elements with pre-fab iron-meta polymer element

--- utils/ScrubberUtils.js
+++
@@ -1,8 +0,0 @@
-var body = document.getElementsByTagName('body');
-console.log(body);
-var scrubber = Polymer.dom('tween-scrubber');
-console.log(scrubber);
-
-var tweens = document.getElementsByTagName('my-tweens')[0];
-console.log(tweens);
-
